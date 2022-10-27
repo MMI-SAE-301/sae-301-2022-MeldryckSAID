62d99ca3ec33f15b095f40c0e50b03f80d098ac0
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2022-10-27 11:27:33 +0200

    ajout des footer

--- src/pages/liste.vue
+++ src/pages/liste.vue
@@ -1,5 +1,33 @@
 <template>
-  <main class="m-0 min-h-screen bg-mclair"></main>
+  <main class="m-0 min-h-screen bg-mclair">
+    <RouterLink
+      :to="{ name: 'montre-edit-id', params: { id: montrex.id_montre } }"
+      v-for="montrex in MesMontre"
+      :key="montrex.id_montre"
+    >
+      <div v-for="montrex in MesMontre" :key="MesMontre.id_montre">
+        <Montrex v-bind="montrex" class="w-32" />
+      </div>
+    </RouterLink>
+    <RouterLink
+      :to="{ name: 'montre-edit-id', params: { id: montrer.id_montre } }"
+      v-for="montrer in MesMontre"
+      :key="montrer.id_montre"
+    >
+      <div v-for="montrer in MesMontrer" :key="MesMontrer.id_montre">
+        <Montrer v-bind="montrer" class="w-32" />
+      </div>
+    </RouterLink>
+    <RouterLink
+      :to="{ name: 'montre-edit-id', params: { id: montrec.id_montre } }"
+      v-for="montrec in MesMontrec"
+      :key="montrec.id_montre"
+    >
+      <div v-for="montrec in MesMontrec" :key="MesMontrec.id_montre">
+        <Montrec v-bind="montrec" class="w-32" />
+      </div>
+    </RouterLink>
+  </main>
   <footer>
     <Foter></Foter>
   </footer>
@@ -7,4 +35,43 @@
 
 <script setup lang="ts">
 import Foter from "@/components/foter.vue";
+
+import type { montrex } from "@/type";
+import type { montrer } from "@/type";
+import type { montrec } from "@/type";
+
+import Montrer from "@/components/Montrer.vue";
+import Montrex from "@/components/Montrex.vue";
+import Montrec from "@/components/Montrec.vue";
+
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { user, supabase } from "../supabase";
+
+const router = useRouter();
+const MesMontre = ref();
+const MesMontrec = ref();
+const MesMontrer = ref();
+
+async function getMontre() {
+  const { error, data } = await supabase.from("montre").select("*");
+
+  console.log(data);
+  MesMontre.value = data;
+}
+async function getMontrec() {
+  const { error, data } = await supabase.from("montrec").select("*");
+
+  console.log(data);
+  MesMontrec.value = data;
+}
+async function getMontrer() {
+  const { error, data } = await supabase.from("montrer").select("*");
+
+  console.log(data);
+  MesMontrer.value = data;
+}
+getMontrer();
+getMontre();
+getMontrec();
 </script>
